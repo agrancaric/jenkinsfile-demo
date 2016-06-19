c36853b4588b3dd62024125988eff824365b0ba6
agrancaric <agrancaric@agrancarics-MacBook-Pro.local>
2016-06-19 12:18:36 +0200

    Jenkinsfile testing first commit

---
+++ src/main/groovy/net/agrancaric/JenkinsBuildDemoApplication.groovy
@@ -0,0 +1,12 @@
+package net.agrancaric
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JenkinsBuildDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run JenkinsBuildDemoApplication, args
+	}
+}
